f8d2220d796b66a75dc7ca794bb10690cc92bee0
1sallyb <157546979+1sallyb@users.noreply.github.com>
2024-05-02 14:35:50 -0500

    Updated requirements and setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "Final Project",
+    version = '0.0.0',
+    author = "Sally Brumage",
+    author_email='sbrumage20@gmail.com',
+    packages = find_packages()
+
+)
