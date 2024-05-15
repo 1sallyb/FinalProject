da781f3d2bddd95af086f5a1041fb86926188507
1sallyb <157546979+1sallyb@users.noreply.github.com>
2024-05-15 18:46:15 -0500

    finished the data transformation class

--- final_project/components/data_transformation.py
+++ final_project/components/data_transformation.py
@@ -91,34 +91,88 @@ class DataTransformation:
             except Exception as e:
                 raise final_except(e,sys)
             
-            def initiate_data_transformation(self)->DataTransformationArtifact:
-                '''
+        def initiate_data_transformation(self)->DataTransformationArtifact:
+            '''
                 initiate data transformation component of pipeline
                 returns a transformed preprocessor object
-                '''
+            '''
 
-                try:
-                    if self.data_validation_artifact.validation_status:
-                        logging.info("Starting data transformation")
+            try:
+                if self.data_validation_artifact.validation_status:
+                    logging.info("Starting data transformation")
 
-                        preprocessor = self.get_data_transformer_object
-                        logging.info('Acquired preprocessor object')
+                    preprocessor = self.get_data_transformer_object
+                    logging.info('Acquired preprocessor object')
 
-                        train_df = DataTransformation.read_data(file_path=self.data_ingestion_artifact.trained_file_path)
-                        test_df = DataTransformation.read_data(file_path=self.data_ingestion_artifact.test_file_path)
+                    train_df = DataTransformation.read_data(file_path=self.data_ingestion_artifact.trained_file_path)
+                    test_df = DataTransformation.read_data(file_path=self.data_ingestion_artifact.test_file_path)
 
-                        input_feature_train_df = train_df.drop(columns = [TARGET_COLUMN], axis = 1)
-                        target_feature_train_df = train_df[TARGET_COLUMN]
+                    input_feature_train_df = train_df.drop(columns = [TARGET_COLUMN], axis = 1)
+                    target_feature_train_df = train_df[TARGET_COLUMN]
 
-                        logging.info("Acquired features of the traning data set")
-                        input_feature_train_df['company_age'] = CURRENT_YEAR - input_feature_train_df['yr_of_estab']
-                        logging.info('Created column company_age in the Training dataset')
+                    logging.info("Acquired features of the traning data set")
+                    input_feature_train_df['company_age'] = CURRENT_YEAR - input_feature_train_df['yr_of_estab']
+                    logging.info('Created column company_age in the Training dataset')
 
-                        drop_cols = self._schema.config['drop_columns']
+                    drop_cols = self._schema.config['drop_columns']
 
-                        logging.info("Dropped the drop_cols from the training dataset")
+                    logging.info("Dropped the drop_cols from the training dataset")
 
-                        input_feature_train_df = drop_columns(df = input_feature_train_df, cols = drop_columns)
+                    input_feature_train_df = drop_columns(df = input_feature_train_df, cols = drop_columns)
+                    target_feature_train_df = target_feature_train_df.replace(TargetValueMapping())
 
-                except Exception as e:
-                    raise final_except(e,sys)
+                    input_feature_test_df = test_df.drop(columns = [TARGET_COLUMN], axis = 1)
+                    target_feature_test_df = test_df[TARGET_COLUMN]
+
+                    input_feature_test_df['company_age'] = CURRENT_YEAR - input_feature_test_df['yr_of_estab']
+
+                    logging.info('Created column company_age in the testing dataset')
+                    logging.info("Dropped the drop_cols from the testing dataset")
+
+                    input_feature_test_df = drop_columns(df = input_feature_test_df, cols = drop_columns)
+                    target_feature_test_df = target_feature_test_df.replace(TargetValueMapping())
+
+                    logging.info("Got train features and test features of testing dataset")
+                    logging.info("Applying preprocessing object on training and testing dataframe")
+
+                    input_features_train_arr = preprocessor.fit_transform(input_feature_train_df)
+                    input_features_test_arr = preprocessor.transform(input_feature_test_df)
+
+                    logging.info("The preprocessor object has applied fit transform to train features")
+                    logging.info("Applying SMOTEENN on Training dataset")
+
+                    smt = SMOTEENN(sampling_strategy = 'minority')
+
+                    input_feature_train_final, target_feature_train_final = smt.fit_resample(
+                        input_feature_train_arr, target_feature_train_df
+                    )
+
+                    logging.info("Applied SMOTEENN")
+                    logging.info("Created train array and test array")
+
+                    train_arr = np.c_[
+                        input_feature_train_final, np.array(target_feature_train_final)
+                    ]
+                    test_arr = np.c_[
+                        input_feature_test_final,np.array(target_feature_test_df)
+                    ]
+
+                    save_object(self.data_transformation_config.transformed_object_file_path, preprocessor)
+                    save_numpy_array_data(self.data_transformation_config.transformed_train_file_path, array = train_arr)
+                    save_numpy_array_data(self.data_transformation_config.transformed_test_file_path, array = test_arr)
+                    
+                    logging.info("Saved preprocessor object")
+                    logging.info("Exited initiate_data_transformation method of DataTransformation class")
+
+                    data_transformation_artifact = DataTransformationArtifact(
+                        transformed_object_file_path = self.data_transformation_config.transformed_object_file_path,
+                        transformed_train_file_path = self.data_transformation_config.transformed_train_file_path,
+                        transformed_test_file_path = self.data_transformation_config.transformed_test_file_path,
+                    )
+
+                    return data_transformation_artifact                    
+                else:
+                    raise Exception(self.data_validation_artifact.message)
+                    
+            except Exception as e:
+                raise final_except(e,sys)
